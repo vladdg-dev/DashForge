fb8acd7a8e6715753dbffe9666c844b8adcfc04e
psypher1337 <vlad.dragusanu@gmail.com>
2024-03-01 20:32:30 +0200

    added header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,14 @@
+import { Layout, Space } from "antd";
 import CurrentUser from "./CurrentUser";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div>
-      <CurrentUser />
-    </div>
+    <Layout.Header className={styles.header}>
+      <Space align="center" size="middle">
+        <CurrentUser />
+      </Space>
+    </Layout.Header>
   );
 };
 
