7ba396db3f5150edc4b09903e96aa567a2dea012
psypher1337 <vlad.dragusanu@gmail.com>
2024-03-04 20:51:57 +0200

    added home component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,16 @@
+import { Col, Row } from "antd";
+
 export const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <Row gutter={[32, 32]} style={{ marginTop: "32px" }}>
+        <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
+          Calendar
+        </Col>
+        <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
+          Dashboard
+        </Col>
+      </Row>
+    </div>
+  );
 };
