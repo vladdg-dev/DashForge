6a8c43062bb8314509ac4e078d390282f72e5e65
psypher1337 <vlad.dragusanu@gmail.com>
2024-03-05 11:37:18 +0200

    added deals chart

--- src/components/home/DealsChart.tsx
+++ src/components/home/DealsChart.tsx
@@ -1,5 +1,78 @@
+import { DollarOutlined } from "@ant-design/icons";
+import { Card } from "antd";
+import { Text } from "../text";
+import { Area, AreaConfig } from "@ant-design/plots";
+import { useList } from "@refinedev/core";
+import { DASHBOARD_DEALS_CHART_QUERY } from "@/graphql/queries";
+import { useMemo } from "react";
+import { mapDealsData } from "@/utilities/helpers";
+import { GetFieldsFromList } from "@refinedev/nestjs-query";
+import { DashboardDealsChartQuery } from "@/graphql/types";
+
 const DealsChart = () => {
-  return <div>Deals Chart</div>;
+  const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
+    resource: "dealStages",
+    filters: [
+      {
+        field: "title",
+        operator: "in",
+        value: ["WON", "LOST"],
+      },
+    ],
+    meta: {
+      gqlQuery: DASHBOARD_DEALS_CHART_QUERY,
+    },
+  });
+
+  const dealData = useMemo(() => {
+    return mapDealsData(data?.data);
+  }, [data?.data]);
+
+  const config: AreaConfig = {
+    data: dealData,
+    xField: "timeText",
+    yField: "value",
+    isStack: false,
+    seriesField: "state",
+    animation: true,
+    startOnZero: false,
+    smooth: true,
+    legend: {
+      offsetY: -6,
+    },
+    yAxis: {
+      tickCount: 4,
+      label: {
+        formatter: (v: string) => {
+          return `$${Number(v) / 1000}k`;
+        },
+      },
+    },
+    tooltip: {
+      formatter: (data) => {
+        return {
+          name: data.state,
+          value: `$${Number(data.value) / 1000}k`,
+        };
+      },
+    },
+  };
+
+  return (
+    <Card
+      style={{ height: "100%" }}
+      title={
+        <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <DollarOutlined />
+          <Text size="sm" style={{ marginLeft: "0.5rem" }}>
+            Deals
+          </Text>
+        </div>
+      }
+    >
+      <Area {...config} height={325} />
+    </Card>
+  );
 };
 
 export default DealsChart;
